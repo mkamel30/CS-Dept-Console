1cada2c8fb850c772439407006db68767f0ce042
Mostafa Kamel <mkamel30@gmail.com>
2025-12-12 00:08:22 +0000

    use-websocket.ts:16  WebSocket connection to 'wss://6000-firebase-studi

--- src/app/portal/components/portal-display.tsx
+++ src/app/portal/components/portal-display.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { doc, updateDoc } from 'firebase/firestore';
